3ded69d483d25196fbc4be2cc418cf992f50ce45
Anita Safar <ancsa0416@gmail.com>
2022-01-05 18:06:39 +0000

    borrower setup done hasname, canborrow tests done

---
+++ Library/src/test/java/BorrowerTest.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BorrowerTest {
+
+    Borrower borrower;
+    Book book;
+
+    @Before
+    public void before() {
+        borrower = new Borrower("Karen");
+        book = new Book("Angels and Demons", "Dan Brown", "Crime");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Karen", borrower.getName());
+    }
+
+    @Test
+    public void canBorrow() {
+        assertEquals(0, borrower.borrowedBookCount());
+    }
+}
